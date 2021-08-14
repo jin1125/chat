6721cdb49afdad3d806741609d60f2d53b0451a1
jin1125 <shogo.jinta@gmail.com>
2021-08-14 19:17:53 +0900

    apply grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import config from '../config.json'
 console.log(config);
 
 export default () => {
-  const [name,setName] = useState('');
+  const [name,setName] = useState('');  
 
   if( config.signInEnabled && name === ''){
     return <SignIn setName={setName}/>;  
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
 
-const Main = () => {
+const useStyles = makeStyles({
+  root: {
+    display: "grid",
+    height: "100vh",
+    gridTemplateRows: "1fr auto",
+  },
+});
+
+const Main = ({name}) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      Main
+    <div className={classes.root}>
+      <MessageList />
+      <MessageInputField name={name}/>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
