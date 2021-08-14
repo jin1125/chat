cca08994be1c03bd1d2aafe0d0c3cc59fa1715f0
jin1125 <shogo.jinta@gmail.com>
2021-08-14 18:13:24 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -43,7 +43,7 @@ export default function SignIn({setName}) {
   const classes = useStyles();
   const [disabled,setDisabled] = useState(true);
   const [string,setString] = useState('');
-  console.log({disabled,string});
+  const [isComposed,setIsComposed] =useState(false);
 
   useEffect(()=>{
     const disabled = string === ''
@@ -69,11 +69,18 @@ export default function SignIn({setName}) {
             autoFocus
             onChange={(e)=>setString(e.target.value)}
             onKeyDown={(e)=>{
-              console.log({key:e.key});
+              if(isComposed) return;
+
               if(e.key === 'Enter'){
                 setName(e.target.value)
                 e.preventDefault()
               }
+            }}
+            onCompositionStart={()=>{
+             setIsComposed(true)
+            }}
+            onCompositionEnd={()=>{
+             setIsComposed(false)
             }}
           />
           <Button
