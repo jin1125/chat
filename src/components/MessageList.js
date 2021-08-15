63bdce5fd3db31cc39ee7e404d265312c209e4f3
jin1125 <shogo.jinta@gmail.com>
2021-08-15 13:00:51 +0900

    display messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,14 @@
 import { makeStyles } from "@material-ui/core/styles";
+import {List} from '@material-ui/core';
 import React, { useEffect, useState } from "react";
 import { messagesRef } from "../firebase";
+import MessageItem from "./MessageItem";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    overflow:'auto',
+    width:'100%',
   },
 });
 
@@ -15,7 +19,7 @@ const MessageList = () => {
   useEffect(() => {
     messagesRef
       .orderByKey()
-      .limitToLast(3)
+      .limitToLast(15)
       .on("value", (snapshot) => {
         const messages = snapshot.val();
         if(messages === null) return;
@@ -27,7 +31,14 @@ const MessageList = () => {
         setMessages(newMessages);
       });
   }, []);
-  return <div className={classes.root}>MessageList</div>;
-};
+  return (
+  <List className={classes.root}>
+    {
+      messages.map(({key,name,text})=>{
+        return <MessageItem key={key} name={name} text={text}>item</MessageItem>
+      })
+    }
+  </List>
+  )};
 
 export default MessageList;
